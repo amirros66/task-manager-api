c5740386380cc4aed359cb508d6049b020e05fe1
amirros66 <143261344+amirros66@users.noreply.github.com>
2024-03-12 13:58:24 +0100

    Add Task table.

--- app/models.py
+++ app/models.py
@@ -13,3 +13,19 @@ class User(Base):
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow)
     disabled = Column(Boolean, default=False)
+
+    tasks = relationship("Task", back_populates="user")
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    description = Column(String, nullable=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    due_date = Column(DateTime, nullable=True)
+    completed = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    user = relationship("User", back_populates="tasks")
